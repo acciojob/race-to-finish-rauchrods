9ade413a8a6931e27e817031d9a822b32e798e72
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-29 16:52:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,42 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+
+let p1 =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("hi1");
+    }, Math.floor(Math.random()*5000) + 1000);
+        
+});
+
+let p2 =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("hi2");
+    }, Math.floor(Math.random()*5000) + 1000);
+});
+
+let p3 =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("hi3");
+    }, Math.floor(Math.random()*5000) + 1000);
+});
+let p4 =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("hi3");
+    }, Math.floor(Math.random()*5000) + 1000);
+});
+let p5 =  new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("hi3");
+    }, Math.floor(Math.random()*5000) + 1000);
+});
+
+window.promises = [];
+promises.push(p1,p2,p3,p4,p5);
+
+
+Promise.any(promises).then((result) => {
+    console.log(result);
+}).catch((err) => {
+    console.log(err);
+});
